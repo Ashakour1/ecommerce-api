bb4eeb0553845ebb5b9b2b042214907875c95397
Ashakuur1 <engshakrayare114@gmail.com>
2024-02-08 03:59:40 +0300

    Refactor Stripe payment route in routes/stripe.js

--- routes/stripe.js
+++ routes/stripe.js
@@ -1,23 +1,27 @@
-import express from 'express';
-import stripe from 'stripe';
-stripe(process.env.STRIPE_SECRET_KEY);
+import express from "express";
 
+import stripePackage from "stripe";
+
+// Initialize Stripe with your secret key
+const stripe = stripePackage(process.env.STRIPE_SECRET_KEY);
 
 const router = express.Router();
 
-router.post("/payment",async(req,res) => {
-    stripe.charges.create({
-        source : req.body.tokenId,
-        amount : req.body.amount,
-        currency : "usd"
-    },(stripeErr,stripeRes) => {
-        if(stripeErr){
-            res.status(500).json(stripeErr);
-        }else{
-            res.status(200).json(stripeRes);
-        }
-    })
-})
-
+router.post("/payment", async (req, res) => {
+  stripe.charges.create(
+    {
+      source: req.body.tokenId,
+      amount: req.body.amount,
+      currency: "usd",
+    },
+    (stripeErr, stripeRes) => {
+      if (stripeErr) {
+        res.status(500).json(stripeErr);
+      } else {
+        res.status(200).json(stripeRes);
+      }
+    }
+  );
+});
 
 export default router;
